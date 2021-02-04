d92f02ec891e7fd4836017f795347cb87cdd5cf5
Omar González Reyes <omargreyes@hotmail.es>
2021-02-04 08:31:12 -0500

    User register

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../models/User') //Utilizamos el schema
+
 router.get('/users/signin', (req,res)=>{
     res.render('users/signin')
 })
@@ -9,4 +11,32 @@ router.get('/users/signup', (req,res)=>{
     res.render('users/signup')
 })
 
+router.post('/users/signup', async (req,res)=>{
+    const {name,email,password,confirmPassword} = req.body;
+    const errors = [];
+    if(password !== confirmPassword){
+        errors.push({text:'Password do not match'})
+    }
+    if (password.length < 4){
+        errors.push({text:'Password must be at least 4 characters'})
+    }
+
+    if(errors.length >0){
+        res.render('users/signup',{errors, name, email, password, confirmPassword})
+    } else{
+        const emailUser = await User.findOne({email: email})
+
+        if(emailUser){
+            //req.flash('error_msg', 'The Email is already used')
+            res.redirect('/users/signup')
+        } else{
+
+            
+            const newUser = new User({name, email, password});
+            newUser.password =  await newUser.encryptPassword(password)
+            await newUser.save()
+            res.redirect('/users/signin')
+        }
+        }
+})
 module.exports = router
