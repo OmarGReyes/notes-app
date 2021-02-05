129aa06a5df1794d87ac9e4355221f74c6c549a3
Omar González Reyes <omargreyes@hotmail.es>
2021-02-05 14:57:06 -0500

    adding atlas DB

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/notes-db-app',{
+//mongoose.connect('mongodb://localhost/notes-db-app',{
+mongoose.connect('mongodb+srv://omargreyes:Coconutella@cluster0.wrghu.mongodb.net/notes-app?retryWrites=true&w=majority',{
     useCreateIndex:true, //Obligatoria
     useNewUrlParser:true, //Obligatoria
     useFindAndModify:false
